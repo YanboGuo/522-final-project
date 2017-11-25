da7296f393ec372a94df5cfc93bd5dec336e0d29
jedimcc <33985516+jedimcc@users.noreply.github.com>
2017-11-25 18:02:09 -0500

    Add files via upload

--- account.java
+++ account.java
@@ -1,20 +1,45 @@
+package ss;
+import java.util.ArrayList;
+import ss.shoppingSystem.*;
 
 public class account {
   
-	public String ID;
+	public String username;
 	public String password;
+		
+
+	public account(String username, String password){
+	       this.username = username;
+	       this.password = password;
+	}
 	
 	public boolean verify(String pd){
-		password = pd;
-		
-		return false;
-		
+		this.password = pd;
+		boolean t = false;
+		new shoppingSystem();
+		for(account c1 : shoppingSystem.arraylist){
+			  if(c1.username == username){
+				 if(c1.password == password){
+					t = true;
+				 }
+			  }
+		}  	
+		return t;		
 	}
 	
    public boolean find(String id){
-		ID = id;
-		//search in the tree
-		return false;
+		this.username = id;
+		boolean t = false;
 		
-	}
+		new shoppingSystem();
+		for(account c1 : shoppingSystem.arraylist){
+			 t = (c1.username == username) ;							
+		}		
+		return t;
+   }
+ 
+   
+   
+   
+   
 }
