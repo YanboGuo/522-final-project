2f8327349b5b02f8d5594a866a2076f7b39b9168
jedimcc <33985516+jedimcc@users.noreply.github.com>
2017-11-25 13:50:04 -0500

    Add files via upload

--- shoppingSystem.java
+++ shoppingSystem.java
@@ -1,4 +1,31 @@
+import java.util.Scanner;
 
 public class shoppingSystem {
+	
+	 public String ID;
+	 public String password;
+    
+	 Scanner kb = new Scanner(System.in);//input from keyboard
+	 
+	 
+	
+	public boolean login(){
+		
+		System.out.println("Input User ID:");
+		ID = kb.nextLine();
+		
+		System.out.println("Input password:");
+		password = kb.nextLine();
 
+		
+		
+		return verify();		
+	}
+	
+	
 }
+
+
+
+
+
