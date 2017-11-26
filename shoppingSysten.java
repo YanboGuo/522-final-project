21e58b5ef5fe846ce953bf47c8535f7ac0c3cf74
Yanbo Guo <amberguo1992@gmail.com>
2017-11-26 11:50:09 -0500

    Add files via upload

--- shoppingSysten.java
+++ shoppingSysten.java
@@ -1,30 +1,39 @@
 //import java.lang.*;
 import java.util.*;
-
+//import java.io.*;
 
 public class shoppingSysten {
+	static shoppingSystem shoppingSysteminstance = new shoppingSystem();
+	private static Scanner systeminput;
 	public static void main(String args[]){
-	boolean redo = true;
-	Scanner systeminput = new Scanner(System.in);
-	shoppingSystem shoppingSysteminstance = new shoppingSystem();
-	
-	System.out.println("Welcome to the New Era online shopping system");
-	System.out.println("Please Select your status: Input 'R' for Register and 'S'for Sign in ");
-	while(redo){
-	String input = systeminput.nextLine();
-	if (input.equals("R")){
-		System.out.println("Register");
-		shoppingSysteminstance.reister();
-		redo = false;
-	}
-	else if (input.equals("S")){
-		System.out.println("Sign in");
-		redo = false;
+		while(true){
+		systeminput = new Scanner(System.in);
+		boolean redo = true;
+					
+		
+		System.out.println("Welcome to the New Era online shopping system");
+		System.out.println("Please Select your status: Input 'R' for Register and 'S'for Sign in ");
+				
+		redo= true;
+		while(redo){
+			System.out.println("-----------");			
+			String input = systeminput.nextLine();
+			if (input.equals("R")){
+				System.out.println("Register");
+				shoppingSysteminstance.register();
+				redo = false;
+			}
+			else if (input.equals("S")){
+				System.out.println("Sign in");
+				redo = false;
+			}
+			else{
+				System.out.println("wrong input, please re-select 'R' for register or 'S' for sign in");
+			}
 		}
-	else{
-		System.out.println("wrong input, please re-select 'R' for register or 'S' for sign in");
-	}
-	}
-	systeminput.close();
+	
+		}
+		
+		
 	}
 }
